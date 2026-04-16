c21851fdefe5eac38a68a6810975915da61657fe
Naomi Nzioka <nziokanaomi422@gmail.com>
2026-04-16 23:28:59 +0300

    added validation

--- server/models.py
+++ server/models.py
@@ -1,4 +1,6 @@
 from config import db
+from sqlalchemy.orm import validates
+import re
 
 class User(db.Model):
     __tablename__ = "users"
@@ -6,7 +8,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String,unique=True, nullable=False)
     email = db.Column(db.String, unique=True)
-    _password_hash = db.Column(db.String, nullable=False)
+    _password_hash = db.Column(db.String)
     monthly_income = db.Column(db.Float, default=0.0)
     monthly_budget=db.Column(db.Float, default=0.0)
     
@@ -15,6 +17,22 @@ class User(db.Model):
     
     def __repr__(self):
         return f'<User {self.username},ID {self.id}>'
+    
+    #add user validation
+    @validates('email', 'monthly_budget', 'monthly_income')
+    def validate_user(self, key, value):
+        email_pattern = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
+        if key == 'email':
+            if value and not re.match(email_pattern, value):
+                raise ValueError('Invalid email address format (e.g., user@example.com)')
+        if key == 'monthly_budget':
+            if value < 0:
+                raise ValueError(f'{key} cannot be a negative number')
+        if key == 'monthly_income':
+            if value < 0:
+                raise ValueError(f'{key} cannot be a negative number')
+            
+        return value
     
 class Expense(db.Model):
     __tablename__ = 'expenses'
@@ -32,3 +50,21 @@ class Expense(db.Model):
     #foreign key linking to every user in Users table
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
     user = db.relationship('User', back_populates='expenses')
+    
+    @validates('amount', 'description', 'title', 'category')
+    def validate_expense(self, key, value):
+        if key == 'amount':
+            if value < 0:
+                raise ValueError('Amount must be a positive number')
+        if key == 'description':
+            if len(value) > 200:
+                raise ValueError('Description should not exceed 200 characters long')
+        if key == 'title':
+            if not value or value.strip() == '':
+                raise ValueError('Title cannot be empty to enable easy tracking of expenses')
+        if key == 'category':
+            if not value or value.strip() == '':
+                raise ValueError('Category cannot be empty')
+            
+        return value
+    
